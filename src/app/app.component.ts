57a39eecf8ca3c306275a4f0f62627cd9edece53
anthod57 <87667439+anthod57@users.noreply.github.com>
2022-05-05 20:14:07 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,27 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'weather-app';
+  searchInput: string
+  city: string
+  error: boolean
+  errorText: string
+
+  onClick() {
+    if(!this.searchInput){
+      this.error = true;
+      this.errorText = "Please enter a valid city name !";
+      return;
+    }else{
+      this.error = false;
+      this.errorText = "";
+      this.city = this.searchInput;
+    }
+  }
+
+  onKeyUp(event: any) {
+    this.searchInput = event.target.value;
+  }
 }
